2164ca326a131da9d3a571adc8ca5b3110e83967
progaurav052 <gpprojects511@gmail.com>
2025-01-31 23:29:11 +0530

    Added code for auth without using middlewares

--- src/app.js
+++ src/app.js
@@ -2,22 +2,38 @@ const express = require("express");
 //i saw from the express code it is exporting something called createapplication function
 const app = express();
 //here we are creating that server application and referencing it to app
-//example for multiple route handlers 
+//example for multiple route handlers
 
-app.get("/user",(req,res,next)=>{
-  console.log("Get Request at /User");
-  res.send("1st Response");
-  next();
-  
+app.get("/admin/getAllUser",(req,res)=>{
+   console.log("Admin Auth started");
+   const token ="xyz";
+   const isAuthorized = token === "xyzvb";
+   if(!isAuthorized)
+   {
+    res.status(401).send("Wrong admin credentials");
+   }
+   else{
+    console.log("Admin authenticated..")
+    res.send("Sent All Users Data...");
 
+   }
 
-},
-  (req,res)=>{
-    console.log("2nd response console");
-    res.send("2nd Response");
+})
+app.get("/admin/DeleteUser",(req,res)=>{
+  console.log("Admin Auth started");
+  const token ="xyz";
+   const isAuthorized = token === "xyzvb";
+   if(!isAuthorized)
+   {
+    res.status(401).send("Wrong admin credentials");
+   }
+   else{
+    console.log("Admin authenticated...");
+    res.send("User Data Deleted by Admin...");
 
-  }
-)
+   }
+
+})
 
 app.listen(3000, () => {
   console.log("Server up and running on port 3000");
