03b379c9cd546eb667949f27efb7f64ecf3629a9
Gaurav <gaurav511pai@gmail.com>
2025-01-31 19:13:08 +0530

    formatting code

--- src/app.js
+++ src/app.js
@@ -1,35 +1,33 @@
 const express = require("express");
-//i saw from the express code it is exporting something called createapplication function 
-const app =express();
-//here we are creating that server application and referencing it to app 
+//i saw from the express code it is exporting something called createapplication function
+const app = express();
+//here we are creating that server application and referencing it to app
 
-app.get("/user/:UserId",(req,res)=>{
-    // for Dynamic calls like /user/7097 ... or anything , searching for particular user
-    console.log(req.params);
-    console.log(req.query);
+app.get("/user/:UserId", (req, res) => {
+  // for Dynamic calls like /user/7097 ... or anything , searching for particular user
+  console.log(req.params);
+  console.log(req.query);
+  res.send(`fetching details of User : ${req.params}`);
+});
 
-    res.send(`fetching details of User : ${req.params}`)
-})
-app.get("/user",(req,res)=>{
-    console.log(req.query);
-    //used to fetch query params ..
+app.get("/user", (req, res) => {
+  console.log(req.query);
+  //used to fetch query params ..
+  res.send({ firstname: "Gaurav", lastname: "Pai" });
+});
 
-    res.send({"firstname":"Gaurav","lastname":"Pai"});
+app.post("/user", (req, res) => {
+  res.send("User Deatils received...storing in database...");
+});
 
-})
-app.post("/user",(req,res)=>{
-    res.send("User Deatils received...storing in database...");
+app.delete("/user", (req, res) => {
+  res.send("User data deleted...");
+});
 
-})
-app.delete("/user",(req,res)=>{
-    res.send("User data deleted...");
-})
-app.use("/user",(req,res)=>{
-    res.send("Redundant response...");
+app.use("/user", (req, res) => {
+  res.send("Redundant response...");
+});
 
-})
-
-app.listen(3000,()=>{
-    console.log("Server up and running on port 3000");
-
+app.listen(3000, () => {
+  console.log("Server up and running on port 3000");
 });
