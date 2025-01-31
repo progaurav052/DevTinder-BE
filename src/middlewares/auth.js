f46181b83a6ea44b2f57f0b6c9d6ba04f7aa8ed1
progaurav052 <gpprojects511@gmail.com>
2025-02-01 00:05:48 +0530

    Created an auth for user , using another synatx/form

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -9,6 +9,18 @@ const AdminAuth = (req, res, next) => {
   }
 };
 
+const UserAuth = (req,res,next) =>{
+  console.log("User Auth started");
+  const token = "xyz";
+  const isAuthorized = token === "xyz";
+  if (!isAuthorized) {
+    res.status(401).send("Wrong User credentials");
+  } else {
+    next();
+  }
+}
+
 module.exports = {
   AdminAuth,
+  UserAuth,
 };
