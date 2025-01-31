8542148cbbcffad09f35a3a6485edd13192707b9
Gaurav <gaurav511pai@gmail.com>
2025-02-01 00:05:48 +0530

    Created an auth for user , using another synatx/form

--- src/app.js
+++ src/app.js
@@ -3,21 +3,29 @@ const express = require("express");
 const app = express();
 //here we are creating that server application and referencing it to app
 //example for multiple route handlers
-const {AdminAuth} =require("./middlewares/auth");
+const { AdminAuth, UserAuth } = require("./middlewares/auth");
 
-app.use("/admin",AdminAuth);
+app.use("/admin", AdminAuth);
+// we can explicity make this middlewares available to the api calls
 
+app.get("/user/login", UserAuth, (req, res) => {
+  //here we used the concept of middlewares as multiple route handlers...
+  //middlwares ~ route handlers
+  res.send("User ops performed successfully");
+});
+app.post("/user/post-data", UserAuth, (req, res) => {
+  res.send("User posted data successfully");
+});
+app.post("/user/register", (req, res) => {
+  res.send("user registered !!! , welcome...");
+});
 
-app.get("/admin/getAllUser",(req,res)=>{
-
-    res.send("Sent All Users Data...");
-
-})
-app.get("/admin/DeleteUser",(req,res)=>{
-  
-    res.send("User Data Deleted by Admin...");
-
-})
+app.get("/admin/getAllUser", (req, res) => {
+  res.send("Sent All Users Data...");
+});
+app.get("/admin/DeleteUser", (req, res) => {
+  res.send("User Data Deleted by Admin...");
+});
 
 app.listen(3000, () => {
   console.log("Server up and running on port 3000");
