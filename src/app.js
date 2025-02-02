34353ecbe99e6ebe62b94e3bcb7ef5e486260d81
Gaurav <gaurav511pai@gmail.com>
2025-02-02 18:45:55 +0530

    Wrote some new api = POST , PATCH , FEED , DELETE , used model for crud ops and refer docs

--- src/app.js
+++ src/app.js
@@ -4,8 +4,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { User } = require("./models/user");
 const { connectDB } = require("./config/database");
+const { ReturnDocument } = require("mongodb");
 
 const app = express();
+
+app.use(express.json());
+// it converts json to js object and makes it available in req.body
+// this is middleware will be activated for all routes
+// middleware to read the json data in the req.body
+// because our server is not able to read the json data which is being sent
+// express doesnt automatically parse json data , beacuse data can be of any type , json , urlencoded etc ...
 
 connectDB()
   .then(() => {
@@ -20,17 +28,106 @@ connectDB()
     console.log("Database connection Not Successfull!!!");
   });
 
+// for finding and updating
+//working
+app.patch("/user", async (req, res) => {
+  const userId = req.body._id;
+  console.log(userId);
+
+  try {
+    // we have to find a record and update it
+    //using findbyID
+    const updated_doc = await User.findByIdAndUpdate(
+      userId,
+      { emailId: "rohit264@gmail.com", password: "rohit264" },
+      { returnDocument: "after" }
+    );
+    if (!updated_doc) {
+      res.status(404).send("Document to update not found!!");
+    } else {
+      res.send(updated_doc);
+    }
+  } catch (error) {
+    //coding error
+    res.status(400).send("Something went Wrong !!!");
+  }
+});
+
+//working
+app.delete("/user", async (req, res) => {
+  //here we will use find by email and delete
+  //we can use findByID also
+  //we will use findone
+  const userEmail = req.body.emailId;
+  try {
+    const deleted_doc = await User.findOneAndDelete({ emailId: userEmail });
+    if (!deleted_doc) {
+      res.status(404).send("document to delete not found !!!");
+    } else {
+      res.send(`Deleted Document is :${deleted_doc}`);
+    }
+  } catch (error) {
+    // coding error
+    res.status(400).send("something went wrong");
+  }
+});
+
+// Feed API --> Get all users
+//working
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("No Users found till now !!!");
+    } else {
+      res.send(users);
+    }
+  } catch (error) {
+    //coding error
+    res.status(400).send("something went Wrong!!!");
+  }
+});
+
+//GET API - > to get specific user
+//working
+app.get("/user", async (req, res) => {
+  // we use the Model for Crud operations
+  const userEmail = req.body.emailId;
+
+  try {
+    //find will return array of users which match emailID
+    const found_user = await User.findOne({ emailId: userEmail });
+    if (!found_user) {
+      res.send("User Not found!!!");
+    } else {
+      res.send(found_user);
+    }
+  } catch (err) {
+    // this is to handle coding error
+    res.status(400).send("Something went Wrong");
+  }
+});
+
+// POST API --> to store data in DB
+//working
 app.post("/signup", async (req, res) => {
+  /*
   const userObject = {
     firstName: "Rohit",
     lastName: "Sharma",
     emailId: "rohitcwc@gmail.com",
-    password: "rohit@123",
-  };
+    password: "rohit@123",  
+    // here we used key value pair 
+    // in json even the key is an string "<>"
+  }
+  
+  */
 
-  const newuser = new User(userObject);
-  await newuser.save();
-  res.send("new User Added Successfully !!!");
+  const newuser = new User(req.body);
+  try {
+    await newuser.save(); // a new doc will be saved in the collection after this
+    res.send("New User added Successfully !!!");
+  } catch (err) {
+    res.status(400).send("Error Saving the user:" + err.message);
+  }
 });
-
-//start our server to listen on port
