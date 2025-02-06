d2541e9846e5cfb13207b9a2019fd687b849b201
progaurav052 <gpprojects511@gmail.com>
2025-02-07 01:42:15 +0530

    Transfered the authentication code using cookie to a middleware and using is it now whereevr reqd

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,26 +1,35 @@
-const AdminAuth = (req, res, next) => {
-  console.log("Admin Auth started");
-  const token = "xyz";
-  const isAuthorized = token === "xyz";
-  if (!isAuthorized) {
-    res.status(401).send("Wrong admin credentials");
-  } else {
-    next();
-  }
-};
+const jwt=require("jsonwebtoken");
+const {User}=require("../models/user");
 
-const UserAuth = (req,res,next) =>{
-  console.log("User Auth started");
-  const token = "xyz";
-  const isAuthorized = token === "xyz";
-  if (!isAuthorized) {
-    res.status(401).send("Wrong User credentials");
-  } else {
+// adding an middleware which will take care of storing user session and safety of APi once logged in 
+const UserAuth = async(req,res,next) =>{
+  //once the user is logged in an jwt willbe created and sent in a coookie of res.send() of /login\
+  
+  try {
+    console.log(req.cookies);
+    const { token } = req.cookies;
+    if (!token) {
+      throw new Error("Login first to get Profile!!");
+    }
+
+    // we have to fetch the correct profile for this
+    const decod_msg = jwt.verify(token, "Dev@123Tinder$5");
+    console.log(decod_msg);
+    const { _id } = decod_msg;
+    if (!_id) {
+      throw new Error("could fecth _id from token!!!");
+    }
+    const user = await User.findById(decod_msg._id);
+    req.user=user;
     next();
+  } catch (error) {
+    res.status(400).send(error.message);
   }
+  
+
 }
 
 module.exports = {
-  AdminAuth,// equivalent to AdminAuth:AdminAuth,
+  //AdminAuth,// equivalent to AdminAuth:AdminAuth,
   UserAuth,// UserAuth:UserAuth,
 };
