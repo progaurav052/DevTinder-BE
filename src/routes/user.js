a05fac581bd21c3aa6bbda72e27ed18d7048326a
progaurav052 <gpprojects511@gmail.com>
2025-02-19 23:06:39 +0530

    Added feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const userRouter = express.Router();
 const { UserAuth } = require("../middlewares/auth");
 const { ConnectionRequest } = require("../models/connectionRequest");
 const USER_SAFE_DATA = "firstName lastName age gender skills";
+const {User} = require("../models/user");
 
 userRouter.get("/user/requestreceived", UserAuth, async (req, res) => {
   // api is to get all the requests that are recived by the user that is logged in
@@ -55,14 +56,43 @@ userRouter.get("/user/connections", UserAuth, async (req, res) => {
       }
       return row.fromUserId;
     });
-    res.status(400).json({
-      data,
-    });
+    res.status(400).json({data});
   } catch (error) {
     res.status(400).send(error.message);
   }
 });
+userRouter.get("/user/feed", UserAuth, async (req, res) => {
+  //api to display feed of users to logged in User
+  try {
+    const loggedInUser = req.user;
+    // we need to get all those users which are not related to user till now
+    //either the user has not ignored or accepted , showed interest till now
+    // sol : All users - users in which user has showed interest/realted
+    const conrequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId"); //from each of those docs select only id
+    // [{},{},{}]
 
+    const hashSetUsers = new Set();
+    conrequests.forEach((req) => {
+      hashSetUsers.add(req.fromUserId);
+      hashSetUsers.add(req.toUserId);
+    });
+
+    const feedData = await User.find({
+      $and :[
+        { _id: { $nin: Array.from(hashSetUsers) } },
+        { _id: { $ne: loggedInUser._id } }
+      ]
+    }
+      
+    ).select(USER_SAFE_DATA);
+    // this will give us our expected users
+    res.status(200).json( feedData );
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
 module.exports = {
   userRouter,
 };
